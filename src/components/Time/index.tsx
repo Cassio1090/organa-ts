b3cc12189f2ca003fe9162a88d9a6042772f2d80
Cassio1090 <cassio-byob@hotmail.com>
2025-04-23 18:13:12 -0300

    adicionando a data de entrada do colaborador

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -26,6 +26,7 @@ const Time = (props: TimeProps) => {
             nome={colaborador.nome}
             cargo={colaborador.cargo}
             imagem={colaborador.imagem}
+            data={colaborador.data}
           />
           )}
       </div>
