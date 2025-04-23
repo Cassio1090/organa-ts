0bf9cf5fcdf8600df5cdc04d95af01421cfd4811
Cassio1090 <cassio-byob@hotmail.com>
2025-04-23 14:49:35 -0300

    subindo projeto organa pro git

---
+++ src/components/MinhaOrganizacao/index.tsx
@@ -0,0 +1,18 @@
+import "./MinhaOrganizacao.css";
+
+
+
+const MinhaOrganizacao = () => {
+  return (
+    <section className="minhaOrganizacao">
+      <h2>Minha Organização:</h2>
+      <img        
+        src="imagens/botao-add.png"
+        alt="imagem botao"
+        className="btnMinhaOrg"
+      ></img>
+    </section>
+  );
+};
+
+export default MinhaOrganizacao;
